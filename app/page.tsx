9839da7805345c56bf125063380b87a129033cf4
Corey Burns <coreyburns@outlook.com>
2023-12-19 15:53:04 -0500

    updated deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-export default function Home() {
-  const Button = () => (
-    <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded">
-      Button
-    </button>
-  );
+"use client";
 
+export default function Home() {
   return (
     <main className="flex min-h-screen border-dashed border-2 border-sky-500">
       <section className="border-dashed border-2 border-orange-600">
         <h1>Hello</h1>
-        <Button />
       </section>
+      <section className="bg-slate-500 h-[20vh] w-[100vw]">2nd section</section>
     </main>
   );
 }
