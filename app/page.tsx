de8789093c2bf721341c4b30a062c99d0d87bcd2
Corey Burns <coreyburns@outlook.com>
2024-01-06 09:57:36 -0500

    updated deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 export default function Home() {
+  const username = "Corey";
+  console.log("Hello World");
+  console.log(`Hello ${username}`);
   return (
     <main className="flex min-h-screen border-dashed border-2 border-sky-500">
       <section className="border-dashed border-2 border-orange-600">
-        <h1>Hello</h1>
+        <h1 className="text-pretty blur-xl">Hello</h1>
+        <h4>Stuff</h4>
       </section>
       <section className="bg-slate-500 h-[20vh] w-[100vw]">2nd section</section>
     </main>
